068699c77ee6af76fd4ed4329e638d7e4756523c
Fortune Allou <aallou@unomaha.edu>
2023-04-21 13:13:46 -0500

    Added layer in Scene.js

--- engine/Scene.js
+++ engine/Scene.js
@@ -1,31 +1,32 @@
-class Scene{
+class Scene {
     gameObjects = []
 
-    constructor(fillStyle){
+    constructor(fillStyle) {
         this.addGameObject(new GameObject("CameraGameObject")
-        .addComponent(new Camera(fillStyle)))
-      }
+            .addComponent(new Camera(fillStyle)))
+    }
 
-    addGameObject(gameObject, translate = Vector2.zero, scale = Vector2.one, rotation = 0){
+    addGameObject(gameObject, translate = Vector2.zero, scale = Vector2.one, rotation = 0, layer = 0) {
         this.gameObjects.push(gameObject);
         gameObject.transform.x = translate.x;
         gameObject.transform.y = translate.y;
         gameObject.transform.sx = scale.x;
         gameObject.transform.sy = scale.y;
         gameObject.transform.r = rotation;
-  
-        if(gameObject.start && !gameObject.started){
+        gameObject.layer = layer;
+
+        if (gameObject.start && !gameObject.started) {
             gameObject.started = true
             gameObject.start()
         }
-  
+
         return gameObject;
     }
 
-    addGameObjectTransform(gameObject, transform = new Transform()){
+    addGameObjectTransform(gameObject, transform = new Transform()) {
         this.gameObjects.push(gameObject);
         gameObject.transform = transform;
-      }
+    }
 }
 
 window.Scene = Scene;
